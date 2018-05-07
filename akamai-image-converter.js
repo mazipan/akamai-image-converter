7d6546e78ce8dc28b4e50e6db82bcb7b30d3705e
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-05-07 15:38:52 +0700

    v0.0.3 change module exports

--- akamai-image-converter.js
+++ akamai-image-converter.js
@@ -1,42 +1,44 @@
-export default {
-  convert(originImage, format, quality, width, height) {
-    let result = originImage
-    let haveParam = false
+module.exports.convert = function (originImage, format, quality, width, height) {
+  let result = originImage
+  let haveParam = false
 
-    result = this._withOutputFormat(
-             this._withQuality(
-             this._withDimension(
-              result, width, height),
-              quality),
-              format)
+  result = this._withOutputFormat(
+           this._withQuality(
+           this._withDimension(
+            result, width, height),
+            quality),
+            format)
 
-    return result
-  },
-  _checkParameter(fullImageUrl) {
-    return fullImageUrl.indexOf('?') >= 0 ? '&' : '?'
-  },
-  _withOutputFormat(fullImageUrl, format){
-    let result = fullImageUrl
-    if (format) {
-      let param = this._checkParameter(result)
-      result = `${result}${param}output-format=${format}`
-    }
-    return result
-  },
-  _withQuality(fullImageUrl, quality){
-    let result = fullImageUrl
-    if (quality) {
-      let param = this._checkParameter(result)
-      result = `${result}${param}output-quality=${quality}`
-    }
-    return result
-  },
-  _withDimension(fullImageUrl, width, height){
-    let result = fullImageUrl
-    if (width && height) {
-      let param = this._checkParameter(result)
-      result = `${result}${param}resize=${width}px:${height}px`
-    }
-    return result
+  return result
+}
+
+module.exports._checkParameter = function (fullImageUrl) {
+  return fullImageUrl.indexOf('?') >= 0 ? '&' : '?'
+}
+
+module.exports._withOutputFormat = function (fullImageUrl, format){
+  let result = fullImageUrl
+  if (format) {
+    let param = this._checkParameter(result)
+    result = `${result}${param}output-format=${format}`
   }
+  return result
+}
+
+module.exports._withQuality = function (fullImageUrl, quality){
+  let result = fullImageUrl
+  if (quality) {
+    let param = this._checkParameter(result)
+    result = `${result}${param}output-quality=${quality}`
+  }
+  return result
+}
+
+module.exports._withDimension = function(fullImageUrl, width, height){
+  let result = fullImageUrl
+  if (width && height) {
+    let param = this._checkParameter(result)
+    result = `${result}${param}resize=${width}px:${height}px`
+  }
+  return result
 }
